7a70467641599b8794164ebf21e7ac784b775bd3
aleena-zahra <155615101+aleena-zahra@users.noreply.github.com>
2024-01-14 13:54:41 +0500

    Update and rename task4.py to geometric_shapes.py

--- geometric_shapes.py
+++ geometric_shapes.py
@@ -2,19 +2,20 @@ from turtle import Turtle,Screen
 import random
 screen = Screen() 
 screen.setup(width=800,height=600)
-screen.title('aleena')
+screen.title('geometric shapes')
 speed = 100
 angle=360
 turtle = Turtle() 
 turtle2 = Turtle()
 #random geometric shapes generated
+#any shape between a triangel and nonagon
 turtle2.goto(200,0)
 for i in range (0,5):
     random_number = random.randint(3,9)
     for i in range (0,random_number):
         turtle.forward(speed)
         turtle.left(angle/random_number)
-#geometric shapes generated where sides increase by 1 each time
+#geometric shapes generated where sides increase by 1 in each iteration
 #going from a triangle upto a nonagon
 turtle2.goto(-200,0)
 for i in range (3,9):
