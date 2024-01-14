d84e6b75c2d81c70683d6c0bfbe666be1d1f66b9
aleena-zahra <155615101+aleena-zahra@users.noreply.github.com>
2024-01-14 14:24:51 +0500

    Update and rename task7.py to polka_dots.py

--- polka_dots.py
+++ polka_dots.py
@@ -3,11 +3,13 @@ import random
 from turtle import Screen
 screen = Screen() 
 screen.setup(width=800,height=600)
-
+#making dots with various different colors on the screen
 turtle_array = []
 colors = [ "red", "pink", "violet", "yellow",  "light blue", "light green"]
-y=-200
+#setting x and y coordinates to the bottom of the screen
+y=-200 
 x=-200
+#using a nested loop
 for dot in range (0,10):
     for dot in range (0,10):
         dot = turtle.Turtle()
@@ -15,9 +17,11 @@ for dot in range (0,10):
         dot.shape("circle")
         dot.color(random.choice(colors))
         dot.goto(x, y)
+        #increasing the y value to shift the dot vertically
         y+=50
         turtle_array.append(dot)
-    #dot.circle(radius=10)
+    #increasing the x value to shift the dot horizontally
+    #changing the y value back to the bottom of the screen
     y=-200    
     x+=50    
     
