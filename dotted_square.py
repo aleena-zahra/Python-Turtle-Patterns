64a88911820c2c620fdef333dc39ccf6b2c9517a
aleena-zahra <155615101+aleena-zahra@users.noreply.github.com>
2024-01-14 13:51:48 +0500

    Update and rename task3.py to dotted_square.py

--- dotted_square.py
+++ dotted_square.py
@@ -3,7 +3,7 @@ import turtle
 speed=20
 angle=90
 thing = turtle.Turtle()
-
+#making a dotted square by using a nested loop and penup and pendown functions
 for i in range (4):
     for j in range (5):
         thing.penup()
